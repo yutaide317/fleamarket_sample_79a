cc262d61e6b99a3f54207817240cf7518b55631b
shintaro-yoshi <shiyoshiyomiro@gmail.com>
2020-10-06 22:13:52 +0900

    routing setting to show "new" view of credit_cards

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :credit_cards, only: [:new]
+
 end
