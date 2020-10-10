c79a72e091fba549d522ea5afd173252ca353975
RYU322 <mk2.loov.cb.four@gmail.com>
2020-10-11 00:42:02 +0900

    user_my_page

---
+++ app/controllers/credit_cards_controller.rb
@@ -0,0 +1,4 @@
+class CreditCardsController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show]
+  resources :credit_cards, only: [:new]
   root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
