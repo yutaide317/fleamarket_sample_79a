b0d5cc47bb6136f8eee4feb97b1bbf1086bcefae
yutaide317 <yutaide317@icloud.com>
2020-10-10 14:16:30 +0900

    validates_add

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,8 @@
 class Profile < ApplicationRecord
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :family_name, presence: true, format: { with: /\A[一-龥ぁ-ん]/}
+  validates :first_name, presence: true, format: { with: /\A[一-龥ぁ-ん]/}
+  validates :family_name_kana, presence: true, format: { with: /\A[ぁ-んー－]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ぁ-んー－]+\z/}
   validates :birth_day, presence: true
   validates :destination_family_name, presence: true
   validates :destination_first_name, presence: true
@@ -12,6 +12,6 @@ class Profile < ApplicationRecord
   validates :prefecture, presence: true
   validates :city, presence: true
   validates :house_number, presence: true
-  validates :phone_number, uniqueness: true
+  validates :phone_number, format: { with: /\A\d{10,11}\z/}
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true, uniqueness: true
+  validates :email, length: { minimum: 7 }, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
   has_one :profile
   accepts_nested_attributes_for :profile
 end
