f37cf70366811fb342378e9e61c23a66edcc2efc
yutaide317 <yutaide317@icloud.com>
2020-10-11 19:54:26 +0900

    routes_change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, only: [:index, :show]
   resources :credit_cards, only: [:new]
-  devise_for :users
+  
   root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
