fb67124d90663186021785a83a88d7d591a65c5a
RYU322 <mk2.loov.cb.four@gmail.com>
2020-10-15 23:47:11 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :items, only: [:new]
+  resources :transactions, only: [:index]
 end
