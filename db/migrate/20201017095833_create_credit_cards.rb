405eb520a7476aeab871de3b8e10e1752c98e688
yutaide317 <yutaide317@icloud.com>
2020-10-19 10:05:59 +0900

    gem_add
    -payjp の導入とdotenv-railsにて秘密鍵の変数指定
    -credit_cardモデルの作成

---
+++ db/migrate/20201017095833_create_credit_cards.rb
@@ -0,0 +1,11 @@
+class CreateCreditCards < ActiveRecord::Migration[6.0]
+  def change
+    create_table :credit_cards do |t|
+      t.integer :user_id,     null: false
+      t.string :customer_id,  null: false
+      t.string :card_id,      null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_063802) do
+ActiveRecord::Schema.define(version: 2020_10_17_095833) do
+
+  create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "customer_id"
+    t.string "card_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "family_name", null: false
@@ -36,7 +44,7 @@ ActiveRecord::Schema.define(version: 2020_10_05_063802) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
