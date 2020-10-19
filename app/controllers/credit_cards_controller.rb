ee9ab5c1105e04e923333f657c52d8caabde59e1
yutaide317 <yutaide317@icloud.com>
2020-10-19 17:30:40 +0900

    form_tag_add
    inputタグをform_tagに変更

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,40 +1,34 @@
 class CreditCardsController < ApplicationController
-
   require "payjp"
-  before_action :set_card
 
-  def new # カードの登録画面。送信ボタンを押すとcreateアクションへ。
-    card = CreditCard.where(user_id: current_user.id).first
-    redirect_to action: "show" if card.present?
+  def new
+    # 後ほど、showアクション(登録クレジットカード詳細表示機能)実装時に追記します。
   end
 
- # indexアクションはここでは省略
+  def create
+    # 前回credentials.yml.encに記載したAPI秘密鍵を呼び出します。
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
 
-  def create #PayjpとCardのデータベースを作成
-    Payjp.api_key = 'PAYJP_PRIVATE_KEY'
-
-    if params['payjp-token'].blank?
-      redirect_to action: "new"
+    # 後ほどトークン作成処理を行いますが、そちらの完了の有無でフラッシュメッセージを表示させます。
+    if params["payjp_token"].blank?
+      redirect_to action: "new", alert: "クレジットカードを登録できませんでした。"
     else
-      # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録します。
+    # 無事トークン作成された場合のアクション(こっちが本命のアクション)
+    # まずは生成したトークンから、顧客情報と紐付け、PAY.JP管理サイトに登録
       customer = Payjp::Customer.create(
-        description: 'test', # 無くてもOK。PAY.JPの顧客情報に表示する概要です。
         email: current_user.email,
-        card: params['payjp-token'], # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐付けて永久保存します。
-        metadata: {user_id: current_user.id} # 無くてもOK。
+        card: params["payjp_token"],
+        metadata: {user_id: current_user.id} #最悪なくてもOK！
       )
+      # 今度はトークン化した情報を自アプリのCredit_cardsテーブルに登録！
       @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      # 無事、トークン作成とともにcredit_cardsテーブルに登録された場合、createビューが表示されるように条件分岐
       if @card.save
-        redirect_to action: "show"
+        #もしcreateビューを作成しない場合はredirect_toなどで表示ビューを指定
       else
         redirect_to action: "create"
       end
     end
   end
 
-  private
-
-  def set_card
-    @card = CreditCard.where(user_id: current_user.id).first if CreditCard.where(user_id: current_user.id).present?
-  end
 end
