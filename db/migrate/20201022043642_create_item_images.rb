87d3e5f9a14bd1406ea0d38495ebfc8a328ea866
shintaro-yoshi <shiyoshiyomiro@gmail.com>
2020-10-22 13:58:19 +0900

    described record names in item_images migration file

--- db/migrate/20201022043642_create_item_images.rb
+++ db/migrate/20201022043642_create_item_images.rb
@@ -1,7 +1,8 @@
 class CreateItemImages < ActiveRecord::Migration[6.0]
   def change
     create_table :item_images do |t|
-
+      t.string :image_src, null: false
+      t.references :item, null: false, foreign_key: true
       t.timestamps
     end
   end
