df42c0d5d230b1279a5725ce2b2b1067bf93ea2f
shintaro-yoshi <shiyoshiyomiro@gmail.com>
2020-10-22 13:54:12 +0900

    described record names in items migration file

--- db/migrate/20201022043530_create_items.rb
+++ db/migrate/20201022043530_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name, null: false
+      t.integer :price, null: false
+      t.text :introduction, null: false
+      t.string :category, null: false 　#readme修正？
+      t.string :brand_id #null: falseではない　#readme修正
+      t.string :item_condition, null: false #readme修正
+      t.string :postage_payer, null: false #readme修正
+      t.string :prefecture, null: false #readme修正
+      t.string :preparation_period, null: false #readme修正
       t.timestamps
     end
   end
