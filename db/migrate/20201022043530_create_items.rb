7b91a630bde5c81897e25d12f87b6353fdbe7c90
shintaro-yoshi <shiyoshiyomiro@gmail.com>
2020-10-22 20:45:13 +0900

    deleted comment out

--- db/migrate/20201022043530_create_items.rb
+++ db/migrate/20201022043530_create_items.rb
@@ -4,12 +4,12 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :name, null: false
       t.integer :price, null: false
       t.text :introduction, null: false
-      t.string :category, null: false 　#readme修正？
-      t.string :brand_id #null: falseではない　#readme修正
-      t.string :item_condition, null: false #readme修正
-      t.string :postage_payer, null: false #readme修正
-      t.string :prefecture, null: false #readme修正
-      t.string :preparation_period, null: false #readme修正
+      t.string :category, null: false
+      t.string :brand_id
+      t.string :item_condition, null: false
+      t.string :postage_payer, null: false
+      t.string :prefecture, null: false
+      t.string :preparation_period, null: false
       t.timestamps
     end
   end
