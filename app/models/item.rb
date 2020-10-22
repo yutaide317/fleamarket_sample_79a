a9bfb66136b1053a3838727a7eee97e7d881784e
shintaro-yoshi <shiyoshiyomiro@gmail.com>
2020-10-22 18:33:26 +0900

    set association between item & item_image model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  belongs to :user
+  has_many :categories
+  has_many :item_images
+  belongs to :brand
 end
--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,2 +1,4 @@
 class ItemImage < ApplicationRecord
+  mount_uploader :src, ImageUploader
+  belongs_to :item
 end
