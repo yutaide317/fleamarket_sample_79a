2a11fe0d9bcc0b499eb1381ba033c1f83dc896e9
shintaro-yoshi <shiyoshiyomiro@gmail.com>
2020-10-23 17:37:11 +0900

    item_params setting

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,21 @@ class ItemsController < ApplicationController
     @item = Item.new
     @item.item_images.new
   end
-  
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :category, :brand_id, :item_condition, :postage_payer, :prefecture, :preparation_period, :price, item_images_attributes: [:image_src])
+  end
+
+
 end
