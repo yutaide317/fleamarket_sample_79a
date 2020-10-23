cbc3af283671548f3a158d012667a16f0125f156
shintaro-yoshi <shiyoshiyomiro@gmail.com>
2020-10-23 17:24:23 +0900

    association fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many :categories
-  accepts_nested_attributes_for :item_images
+  has_many :item_images, dependent: :destroy
+  accepts_nested_attributes_for :item_images, allow_destroy: true
   belongs_to :brand
 end
