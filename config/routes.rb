b997662a561313085dfd607572fedd6026b80c71
shintaro-yoshi <shiyoshiyomiro@gmail.com>
2020-10-24 00:16:21 +0900

    if user signed user can check his selling products setting

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:index, :show]
   resources :credit_cards, only: [:new]
+  root 'items#index'
   resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
