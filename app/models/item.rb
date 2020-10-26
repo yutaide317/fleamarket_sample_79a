03b78c7afaa986914a754ba6b00bb47f2d25e8a1
shintaro-yoshi <shiyoshiyomiro@gmail.com>
2020-10-26 19:13:19 +0900

    items table validation setting

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,18 @@ class Item < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   has_one :purchase
+
+  
+  with_options presence: true do
+    validates :images
+    validates :category
+    validates :item_condition
+    validates :postage_payer
+    validates :prefecture
+    validates :preparation_period
+    validates :name,                length: { maximum: 40 }
+    validates :introduction,        length: { maximum: 1000 }
+    validates :price,               numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  end
+  validates :brand_id, length: { maximum: 20 }, allow_blank: true
 end
