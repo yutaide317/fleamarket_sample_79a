a56ded8ea956bdb0540dd98777e9e3800e226f28
shintaro-yoshi <shiyoshiyomiro@gmail.com>
2020-10-27 22:23:54 +0900

    all validation test executed

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,15 @@
 FactoryBot.define do
   factory :item do
-    images              {"IMG_0152.jpg"}
     name                {"ハンチョウ全巻"}
     introduction        {"漫画『ハンチョウ』全巻です"}
-    category            {"5"}
-    item_condition      {"2"}
-    postage_payer       {"2"}
-    prefecture          {"1"}
-    preparation_period  {"1"}
-    price               {"5000"}
+    category            { 5 }
+    item_condition      { 2 }
+    postage_payer       { 2 }
+    prefecture          { 1 }
+    preparation_period  { 1 }
+    price               { 5000 }
+    after(:build) do |item|
+      item.images << build(:image, item: item)
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Item do
+  describe '#create' do
+    # 1.imagesと name, introduction, category, item_condition, postage_payer, prefecture, preparation_period, price が存在すれば出品登録できること
+    it "is valid with images, name, introduction, category, item_condition, postage_payer, prefecture, preparation_period & price" do
+      item = build(:item)
+      expect(item).to be_valid
+    end
+  end
 end
