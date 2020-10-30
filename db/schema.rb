a0c9b566504b8f8e4855d5e1cc992cf2e005b749
tysickw <68460589+tysickw@users.noreply.github.com>
2020-10-30 22:24:24 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_10_26_121700) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "ancestry"
