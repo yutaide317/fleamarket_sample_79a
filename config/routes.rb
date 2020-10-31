51986c31ac7a39f12a407fd918fadb1fe9ac73ed
tysickw <68460589+tysickw@users.noreply.github.com>
2020-10-31 14:28:46 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   resources :credit_cards, only: [:new]
   root 'items#index'
-  resources :items
+  resources :items do
+    collection do
+      get :search
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :transactions, only: [:index]
 end
