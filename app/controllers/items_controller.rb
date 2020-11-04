f9097ddc89681822474a2f4d7094774c28d206b1
RYU322 <mk2.loov.cb.four@gmail.com>
2020-11-04 21:53:34 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,7 +37,6 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
     if @item.user_id == current_user.id
       if @item.destroy
         redirect_to root_path
