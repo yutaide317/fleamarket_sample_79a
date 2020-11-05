44f83db82cda1fd2f00baa49d465def4780da007
tysickw <68460589+tysickw@users.noreply.github.com>
2020-11-06 00:15:39 +0900

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 require("../items")
-require('item/category')
+require("../category")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
