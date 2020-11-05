38143b46e580fc5dfad7133824038190e98f3d57
tysickw <68460589+tysickw@users.noreply.github.com>
2020-11-06 00:14:27 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :items do
     collection do
-      get :search
+    get 'category/get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
+    get 'category/get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
