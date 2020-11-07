42aac9192e8c5113f7c9ad23095b3c320345f166
yutaide317 <yutaide317@icloud.com>
2020-11-07 19:54:10 +0900

    validates_add

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,5 +1,5 @@
 class CreditCard < ApplicationRecord
   belongs_to :user
 
-  # validates :payment_card_no, presence: { message: 'カード番号を入力してください'}
+  validates :card_id, presence: true
 end
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,18 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Purchase, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe Purchase do
+  describe '#create' do
+    # 1.user_idなしでは登録できない
+    it "is invalid without a user_id" do
+      purchase = build(:purchase, user_id: nil)
+      purchase.valid?
+      expect(purchase.errors[:user_id]).to include("を入力してください")
+    end
+    # 2.item_idなしでは登録できない
+    it "is invalid without a item_id" do
+      purchase = build(:purchase, item_id: nil)
+      purchase.valid?
+      expect(purchase.errors[:item_id]).to include("を入力してください")
+    end
+  end
+end
