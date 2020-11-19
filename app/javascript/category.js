559fa341c6a1de8d7610f0b579d3cb9fbfc07e56
shintaro-yoshi <shiyoshiyomiro@gmail.com>
2020-11-20 01:10:04 +0900

    fix children category form

--- app/javascript/category.js
+++ app/javascript/category.js
@@ -35,6 +35,7 @@ $(document).on('DOMContentLoaded', function(){
     // 親カテゴリー選択後のイベント
     $('#parent_category').on('change', function(){
       const parent_category_id = document.getElementById('parent_category').value; //選択された親カテゴリーの名前を取得
+
       if (parent_category_id != "---"){ //親カテゴリーが初期値でないことを確認
         $.ajax({
           url: '/items/category/get_category_children',
@@ -63,7 +64,7 @@ $(document).on('DOMContentLoaded', function(){
     // 子カテゴリー選択後のイベント
     $('.FormGroup__content__category').on('change', '#child_category', function(){
       const child_category_id = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得
-      console.log(test);
+      console.log(child_category_id);
       if (child_category_id != "---"){ //子カテゴリーが初期値でないことを確認
         $.ajax({
           url: '/items/category/get_category_grandchildren',
