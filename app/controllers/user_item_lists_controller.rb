0094248191831723717a9eb38ffb10d99bb0255c
RYU322 <mk2.loov.cb.four@gmail.com>
2020-11-28 16:20:55 +0900

    Delete user_item_lists_controller.rb

--- app/controllers/user_item_lists_controller.rb
+++
@@ -1,8 +0,0 @@
-# class UserItemListsController < ApplicationController
-#   def index
-#     @items = Item.includes(:images).where(user: current_user).order('created_at DESC')
-#   end
-
-#   def show
-#   end
-# end
