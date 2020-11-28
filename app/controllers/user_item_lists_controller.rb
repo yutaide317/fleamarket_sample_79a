dc159b8420afb0e2d0c3b4c9c80256b66dc3bdf2
RYU322 <mk2.loov.cb.four@gmail.com>
2020-11-28 14:47:42 +0900

    Update user_item_lists_controller.rb

--- app/controllers/user_item_lists_controller.rb
+++ app/controllers/user_item_lists_controller.rb
@@ -1,8 +1,8 @@
-class UserItemListsController < ApplicationController
-  def index
-    @items = Item.includes(:images).where(user: current_user).order('created_at DESC')
-  end
+# class UserItemListsController < ApplicationController
+#   def index
+#     @items = Item.includes(:images).where(user: current_user).order('created_at DESC')
+#   end
 
-  def show
-  end
-end
+#   def show
+#   end
+# end
